86ee76d0902e5c2b2ddb5496f57af4a573c6f62c
El-Juns <90078360+El-Juns@users.noreply.github.com>
2021-09-07 21:53:30 -0500

    Creacion de relaciones

--- src/models/vehiculo.model.ts
+++ src/models/vehiculo.model.ts
@@ -1,4 +1,9 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, belongsTo, hasMany} from '@loopback/repository';
+import {Marca} from './marca.model';
+import {Categoria} from './categoria.model';
+import {CategoriaVehiculo} from './categoria-vehiculo.model';
+import {Foto} from './foto.model';
+import {Proveedor} from './proveedor.model';
 
 @model()
 export class Vehiculo extends Entity {
@@ -52,6 +57,17 @@ export class Vehiculo extends Entity {
   })
   estado?: boolean;
 
+  @belongsTo(() => Marca, {name: 'tiene_marca'})
+  id_marca: number;
+
+  @hasMany(() => Categoria, {through: {model: () => CategoriaVehiculo, keyFrom: 'id_vehiculo', keyTo: 'id_categoria'}})
+  categorias: Categoria[];
+
+  @hasMany(() => Foto, {keyTo: 'id_vehiculo'})
+  fotos: Foto[];
+
+  @belongsTo(() => Proveedor, {name: 'proveedor'})
+  id_proveedor: number;
 
   constructor(data?: Partial<Vehiculo>) {
     super(data);
